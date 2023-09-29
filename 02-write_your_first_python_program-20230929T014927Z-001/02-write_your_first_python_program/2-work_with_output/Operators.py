32f4519df4c5d7efef6370a0a3e714ba2bc67734
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2023-09-28 23:00:04 -0300

    Add files via upload

---
+++ 02-write_your_first_python_program-20230929T014927Z-001/02-write_your_first_python_program/2-work_with_output/Operators.py
@@ -0,0 +1,5 @@
+# <left side> <operator> <right side>
+
+left_side = 10
+right_side = 5
+left_side / right_side # 2
---
+++ 02-write_your_first_python_program-20230929T014927Z-001/02-write_your_first_python_program/2-work_with_output/date.py
@@ -0,0 +1,3 @@
+from datetime import date #importando o modulo date 
+date.today() # chamando a função date 
+print(date.today()) # usando a função print para mostrar o conteúdo de date.today
