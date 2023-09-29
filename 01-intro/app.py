bcfc06f8f1a413d90d2476749842fc733990746c
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2023-09-28 23:07:18 -0300

    Add files via upload

---
+++ 01-intro/app.py
@@ -0,0 +1,41 @@
+# Bem vindo(a). Nesse curso irei estudar os fundamentos de python:
+
+Introdução à Programação e Python:
+Explica o que é programação e por que é útil.
+Apresenta Python como uma linguagem de programação de alto nível e fácil de aprender.
+Destaca a popularidade e a aplicabilidade do Python em diversos campos.
+
+Configuração do Ambiente de Desenvolvimento:
+Instruções para instalar Python no seu sistema operacional.
+Introdução ao uso de ambientes virtuais para gerenciar pacotes e dependências.
+
+Primeiros Passos com Python:
+Noções básicas sobre como escrever e executar código Python.
+Aprender a usar a função print() para exibir saída na tela.
+Variáveis e tipos de dados simples, como números inteiros, ponto flutuante e strings.
+
+Estruturas de Controle:
+Introdução às estruturas de controle, como if, else e while, para tomada de decisões e iteração.
+Exemplos práticos de como usar estruturas de controle em Python.
+
+Coleções de Dados:
+Apresentação de listas, tuplas e dicionários como estruturas de dados em Python.
+Demonstração de como acessar, adicionar e manipular itens em coleções de dados.
+
+Funções e Modularização:
+Compreensão de como definir e chamar funções em Python.
+Explicação da modularização do código e da reutilização de funções.
+
+Manipulação de Arquivos:
+Como abrir, ler e escrever em arquivos usando Python.
+Tratamento de exceções ao trabalhar com arquivos.
+
+Introdução a Bibliotecas e Módulos:
+Destaque para a importância das bibliotecas em Python.
+Exemplos de como importar e usar módulos para realizar tarefas específicas.
+
+Exercícios Práticos:
+Desafios e exercícios práticos ao longo do curso para aplicar os conceitos aprendidos.
+
+Projeto Inicial:
+Um projeto ara consolidar os conhecimentos adquiridos até o momento.
