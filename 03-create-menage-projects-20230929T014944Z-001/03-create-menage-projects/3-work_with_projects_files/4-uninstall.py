0d43341a9a03a8f3ca061926ea896a2845632d0d
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:05:17 -0300

    Delete 03-create-menage-projects-20230929T014944Z-001 directory

--- 03-create-menage-projects-20230929T014944Z-001/03-create-menage-projects/3-work_with_projects_files/4-uninstall.py
+++
@@ -1,9 +0,0 @@
-# desinstalando um pacte
-
-pip uninstall python-dateutil
-
-# desinstalando os requerimentos
-
-pip freeze > requirements.txt
-
-pip uninstall -r requirements.txt -y
--- 03-create-menage-projects-20230929T014944Z-001/03-create-menage-projects/3-work_with_projects_files/5-update.py
+++
@@ -1,7 +0,0 @@
-# simulando estar em uma versão anterior
-
-pip install python-dateutil==2.7.4
-
-# instalar para a versão mais recente. 
-
-pip install --upgrade:
