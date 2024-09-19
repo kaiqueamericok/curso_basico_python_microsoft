61a17c6aab6ab97017381af868ec35edb853f1ee
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:08:53 -0300

    Add files via upload

---
+++ 03-create-menage-projects/03-create-menage-projects/3-work_with_projects_files/3-update.py
@@ -0,0 +1,15 @@
+# com o comando abaixo podemos buscar pela versão mais recente por tentativa e erro. 
+
+pip install python-dateutil==1.4
+
+# com o comando abaixo, conseguimos listar as versões existentes de um pacote
+
+pip install python-dateutil==randomwords
+
+# o comando abaixo verifica versões já instalads por nóes
+
+pip freeze python-dateutil
+
+# atualizar um pacote
+
+pip install <name of package> --upgrade
