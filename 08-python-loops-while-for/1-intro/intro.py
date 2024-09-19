86acdad6e4d8f4ccee41895a8f95be04cc72b4b1
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:06:26 -0300

    Delete 08-python-loops-while-for directory

--- 08-python-loops-while-for/1-intro/intro.py
+++
@@ -1,5 +0,0 @@
-#In this module, the concepts below will be explored
-
-#Identify when to use while and for loops.
-#Perform a task multiple times using while loops.
-#Loop over data in a list using for loops.
