5eec24bfceef1759be685b84a26ee2f5f282a429
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:05:09 -0300

    Delete 02-write_your_first_python_program-20230929T014927Z-001/02-write_your_first_python_program directory

--- 02-write_your_first_python_program-20230929T014927Z-001/02-write_your_first_python_program/1-intro/python.py
+++
@@ -1,6 +0,0 @@
-#In this module, the concepts below will be explored
-
-#Use functions to manage inputs and outputs to the console
-#Create variables to store data
-#Distinguish between data types
-#Use type casting to convert between data types
