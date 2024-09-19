028e6718c66d739631c1e39ee91f332e1d005617
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:10:38 -0300

    Delete 03-create-menage-projects/03-create-menage-projects directory

--- 03-create-menage-projects/03-create-menage-projects/3-work_with_projects_files/1-freeze.py
+++
@@ -1,3 +0,0 @@
-# o comando vai colocar o conteúdo de "freeze" e jogar para a saída do "requirementes.txt"
-
-pip freeze > requirements.txt
--- 03-create-menage-projects/03-create-menage-projects/4-set-up-project/env/1-create_env.py
+++
@@ -1,5 +0,0 @@
-# entrando na pasta scripts pelo terminal 
-
-cd C:\curso-basico-python-microsoft\03-create-menage-projects\4-set-up-project\env\Scripts
-
-activate 
