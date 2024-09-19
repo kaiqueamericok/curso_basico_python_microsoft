94e5f883dbe2204c7d5026355eb3c6478d631728
kaiqueamericok <101136374+kaiqueamericok@users.noreply.github.com>
2024-09-19 09:12:22 -0300

    Add files via upload

---
+++ 03-create-menage-projects/3-work_with_projects_files/1-freeze.py
@@ -0,0 +1,3 @@
+# o comando vai colocar o conteúdo de "freeze" e jogar para a saída do "requirementes.txt"
+
+pip freeze > requirements.txt
---
+++ 03-create-menage-projects/4-set-up-project/env/1-create_env.py
@@ -0,0 +1,5 @@
+# entrando na pasta scripts pelo terminal 
+
+cd C:\curso-basico-python-microsoft\03-create-menage-projects\4-set-up-project\env\Scripts
+
+activate 
